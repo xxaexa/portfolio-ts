8576b3f19e95dfc5b6f10270679c6966e357ec54
andre <akun9396@gmail.com>
2023-08-21 20:59:41 +0700

    remove border

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="w-full h-12 mt-16 border-2 items-center flex justify-center">
+    <div className="w-full h-12 mt-16 items-center flex justify-center">
       <p>made with love</p>
     </div>
   )
